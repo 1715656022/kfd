01bd7f5ac8ea534ba0b899b1602d97119a861ef1
mengpp <mengpp0724@foxmail.com>
2019-09-29 09:16:29 +0800

    add

---
+++ kdf_parent/kdf_etl/src/main/java/com/kdf/cloud/SpringKdfEtlApplication.java
@@ -0,0 +1,13 @@
+package com.kdf.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringKdfEtlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringKdfEtlApplication.class, args);
+	}
+
+}
