c42a2fd62a99321e5a6874b8a48aa55001b9da52
java_1715656022 <1715656022@qq.com>
2019-10-09 09:55:58 +0800

    hhh

--- kdf_parent/kdf_etl_boot/src/main/java/com/kdf/etl/contoller/TestController2.java
+++ kdf_parent/kdf_etl_boot/src/main/java/com/kdf/etl/contoller/TestController2.java
@@ -1,9 +1,10 @@
 package com.kdf.etl.contoller;
 
 import java.io.IOException;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.List;
-
-import javax.annotation.Resource;
+import java.util.Map;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
@@ -22,11 +23,14 @@ import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.hadoop.hbase.HbaseSynchronizationManager;
+import org.springframework.data.hadoop.hive.HiveClient;
+import org.springframework.data.hadoop.hive.HiveClientCallback;
 import org.springframework.data.hadoop.hive.HiveTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.beust.jcommander.internal.Maps;
 import com.kdf.etl.base.BaseHadoop;
 import com.kdf.etl.hadoop.MyMapper;
 
@@ -79,9 +83,34 @@ public class TestController2 extends BaseHadoop {
 
 		return "pook";
 	}
+	@GetMapping("/test2")
+	public void testHive() {
+		String  hiveSql = "select * from my_test_hive";
+		
+//		hiveTemplate.executeScript(scripts);
+		Map maaaa = hiveTemplate.execute(new HiveClientCallback<Map>() {
 
-	public static void main(String[] args) throws Exception {
+			@Override
+			public Map doInHive(HiveClient hiveClient) throws Exception {
+				Map map = Maps.newHashMap();
 
+				java.sql.Connection conn = hiveClient.getConnection();
+
+				Statement stmt = conn.createStatement();
+				ResultSet rs = stmt.executeQuery(hiveSql);
+				// 处理数据
+				while (rs.next()) {
+					String clientType = rs.getString("clientType");
+					System.out.println(clientType);
+					map.put("clientType", clientType);
+					map.put("url", map.get("url"));
+				}
+				return map;
+			}
+
+		});
+		
+		System.out.println("111111111111111111111"+maaaa);
 	}
 
 	private static Connection connection;
