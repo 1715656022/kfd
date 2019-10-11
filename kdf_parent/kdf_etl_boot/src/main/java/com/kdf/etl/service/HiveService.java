9e4b5ff0e19d37b7111b5861204f2eba3c12bed0
java_1715656022 <1715656022@qq.com>
2019-10-11 15:09:21 +0800

    list commot

--- kdf_parent/kdf_etl_boot/src/main/java/com/kdf/etl/service/HiveService.java
+++ kdf_parent/kdf_etl_boot/src/main/java/com/kdf/etl/service/HiveService.java
@@ -2,7 +2,6 @@ package com.kdf.etl.service;
 
 import java.sql.ResultSet;
 import java.sql.Statement;
-import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,7 +10,6 @@ import org.springframework.data.hadoop.hive.HiveClientCallback;
 import org.springframework.data.hadoop.hive.HiveTemplate;
 import org.springframework.stereotype.Service;
 
-import com.beust.jcommander.internal.Lists;
 import com.google.common.collect.Maps;
 
 import lombok.extern.slf4j.Slf4j;
