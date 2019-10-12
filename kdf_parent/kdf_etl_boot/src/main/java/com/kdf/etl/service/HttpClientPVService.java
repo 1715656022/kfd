fc4ba018209cfdcae96838e25f8cd149feba3940
ly <337554812@qq.com>
2019-10-12 09:51:11 +0800

    更改代码

--- kdf_parent/kdf_etl_boot/src/main/java/com/kdf/etl/service/HttpClientPVService.java
+++ kdf_parent/kdf_etl_boot/src/main/java/com/kdf/etl/service/HttpClientPVService.java
@@ -1,12 +1,25 @@
 package com.kdf.etl.service;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
 import java.util.Objects;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.hadoop.hive.HiveClient;
+import org.springframework.data.hadoop.hive.HiveClientCallback;
+import org.springframework.data.hadoop.hive.HiveTemplate;
 import org.springframework.stereotype.Service;
 
+import com.beust.jcommander.internal.Lists;
+import com.google.common.collect.Maps;
 import com.kdf.etl.bean.PvHttpClient;
+import com.kdf.etl.bean.UvTimeDistribution;
 import com.kdf.etl.repository.HttpClientPVRepository;
+import com.kdf.etl.utils.DateUtils;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -22,28 +35,49 @@ import lombok.extern.slf4j.Slf4j;
 @Service
 public class HttpClientPVService {
 	
-	private Long size = 0l;
-	private Long one = 1l;
+	@Autowired
+	private HiveTemplate hiveTemplate;
 
 	@Autowired
 	private HttpClientPVRepository httpClientPVRepository;
 
-	/**
-	 *   用户pv总统计
-	 * @param pvHttpClient
-	 */
-	public void save(PvHttpClient pvHttpClient) {
-		log.info("===============数据pvHttpClient执行开始==============\"");
-		PvHttpClient bean = httpClientPVRepository.findByMethod(pvHttpClient.getMethod());
-		if (Objects.nonNull(bean)) {
-			pvHttpClient.setPvCount(bean.getPvCount()+one);
-			httpClientPVRepository.save(bean);
-		} else {
-			pvHttpClient.setPvCount(size);
+	public void saveHttpClient(String yearMonthDayHour) {
+		log.info("star->saveHttpClient:yearMonthDayHour={}", yearMonthDayHour);
+
+		String sql = "select appid,method,count(1) as pv_count from aapv_log_hive_" + yearMonthDayHour + " group by appid, method";
+		log.info("sql={}", sql);
+
+		List<Map<String, String>> list = hiveTemplate.execute(new HiveClientCallback<List<Map<String, String>>>() {
+			@Override
+			public List<Map<String, String>> doInHive(HiveClient hiveClient) throws Exception {
+				List<Map<String, String>> list = Lists.newArrayList();
+
+				Connection conn = hiveClient.getConnection();
+				Statement stmt = conn.createStatement();
+				ResultSet rs = stmt.executeQuery(sql);
+
+				Map<String, String> map = Maps.newHashMap();
+				while (rs.next()) {
+					map.keySet().removeIf(key -> key != "1");
+					map.put("appid", rs.getString("appid"));
+					map.put("pvCount", rs.getString("pv_count"));
+					map.put("method", rs.getString("method"));
+					list.add(map);
+				}
+				return list;
+			}
+		});
+
+		list.forEach(m -> {
+			PvHttpClient pvHttpClient = new PvHttpClient();
+			pvHttpClient.setAppid(m.get("appid"));
+			pvHttpClient.setMethod(m.get("method"));
+			pvHttpClient.setPvCount( Long.parseLong( m.get("pvCount") ) );
+			pvHttpClient.setCreateTime(new Date());
 			httpClientPVRepository.save(pvHttpClient);
-		}
-		
-		log.info("===============数据pvHttpClient执行完成==============" + pvHttpClient);
+		});
+
+		log.info("end->saveHttpClient");
 	}
 
 }
