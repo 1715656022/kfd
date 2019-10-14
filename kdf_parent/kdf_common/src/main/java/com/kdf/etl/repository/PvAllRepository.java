dc5556e528936760213ef94b9fa13ba47d5df0ac
youyou227 <1974902342@qq.com>
2019-10-14 16:43:45 +0800

    pvall换位置

--- kdf_parent/kdf_common/src/main/java/com/kdf/etl/repository/PvAllRepository.java
+++ kdf_parent/kdf_common/src/main/java/com/kdf/etl/repository/PvAllRepository.java
@@ -1,6 +1,10 @@
 package com.kdf.etl.repository;
 
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.kdf.etl.bean.PvAll;
@@ -8,4 +12,6 @@ import com.kdf.etl.bean.PvAll;
 @Repository
 public interface PvAllRepository extends JpaRepository<PvAll, Long> {
 
+	@Query(value="select count(1) as cnt, date_format(request_time, '%Y-%m-%d %h') as time from kfd_pv_all where appid = ? group by date_format(request_time, '%Y-%m-%d %h')",nativeQuery = true)
+	List<Map<String, Object>> findAllPvMap(String appid);
 }
--- kdf_parent/kdf_web/src/main/java/com/kdf/web/server/service/impl/PvAllServiceImpl.java
+++ kdf_parent/kdf_web/src/main/java/com/kdf/web/server/service/impl/PvAllServiceImpl.java
@@ -1,25 +1,18 @@
 package com.kdf.web.server.service.impl;
 
-import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.kdf.web.server.bean.PvAll;
-import com.kdf.web.server.repository.PvAllRepository;
+import com.kdf.etl.repository.PvAllRepository;
 import com.kdf.web.server.service.PvAllService;
 @Service
 public class PvAllServiceImpl implements PvAllService {
 
 	@Autowired
 	private PvAllRepository pvAllRepository;
-
-	@Override
-	public List<PvAll> getPvAllList(String appid, Date startTime, Date endTime) {
-		return pvAllRepository.findAllByAppidAndRequestTimeBetween(appid, startTime, endTime);
-	}
 
 	@Override
 	public List<Map<String, Object>> findAllPvMap(String appid) {
